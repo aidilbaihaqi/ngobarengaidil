36932d3ac43f597c8d6c67c00baa8c0083362c16
Aidil Baihaqi <aidilmusirjun@gmail.com>
2025-01-30 11:24:32 +0700

    atur eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,16 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    overrides: [
+      {
+        files: ["*.config.js", "*.config.cjs"],
+        rules: {
+          "@typescript-eslint/no-var-requires": "off",
+        },
+      },
+    ],
+  },
 ];
 
 export default eslintConfig;
