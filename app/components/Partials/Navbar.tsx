7cca0b24f20a0aacd822e28b5ff56ee6e155595f
Aidil Baihaqi <aidilmusirjun@gmail.com>
2025-11-10 16:37:39 +0700

    update navbar

--- app/components/Partials/Navbar.tsx
+++ app/components/Partials/Navbar.tsx
@@ -9,7 +9,6 @@ const navLinks = [
   {name: 'Home', href: '/'},
   {name: 'About', href: '/about'},
   {name: 'Project', href: '/projects'},
-  {name: 'Blog', href: '/blogs'},
   {name: 'Contact', href: '/contact'}
 ];
 
